da584bbaee627d919326ed9b378a39914e032217
MohammadNur01 <mohammadnurrossifumi@gmail.com>
2021-09-29 15:05:24 +0700

    add section(content) and deleted header.php and footer.php

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -12,9 +12,8 @@ class Pages extends BaseController
       'title' => 'Home | MH Media Corp',
       'tes' => ['satu', 'dua', 'tiga']
     ];
-    echo view('layout/header', $data);
-    echo view('pages/home');
-    echo view('layout/footer');
+
+    return view('pages/home', $data);
   }
 
   public function about()
@@ -22,8 +21,6 @@ class Pages extends BaseController
     $data =  [
       'title' => 'About Me'
     ];
-    echo view('layout/header', $data);
-    echo view('pages/about');
-    echo view('layout/footer');
+    return view('pages/about', $data);
   }
 }
