ab54e6e511f7c09b3fa88a2d9cc6ce82bc0fbc95
MohammadNur01 <mohammadnurrossifumi@gmail.com>
2021-10-01 21:27:42 +0700

    add delete function

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,7 +33,9 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Pages::index');
 
-// $routes->get('/komik/(:segment)', 'Komik::detail/$1');
+$routes->get('/komik/create', 'Komik::create');
+$routes->delete('/komik/(:num)', 'Komik::delete/$1');
+$routes->get('/komik/(:any)', 'Komik::detail/$1');
 
 
 
--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -79,4 +79,12 @@ class Komik extends BaseController
 
     return redirect()->to('/komik');
   }
+
+  public function delete($id)
+  {
+    $this->komikModel->delete($id);
+    session()->setFlashdata('pesan', 'Data berhasil dihapus');
+
+    return redirect()->to('/komik');
+  }
 }
